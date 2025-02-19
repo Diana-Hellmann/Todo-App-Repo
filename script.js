b7f5bae65e1c97750868d04013d85c24882a3238
Diana Hellmann <hellmanndiana@googlemail.com>
2025-02-19 12:23:58 +0100

    Funktionen

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-const buttons = document.querySelectorAll(".btn-add");
+//Funktionen auslagern
+function createTask(){
+    const neuesLi = document.createElement("li");
+    //damit direkt beschreibbar
+    neuesLi.setAttribute("contentEditable", "true");
+    // X-Button hinzufügen
+    neuesLi.innerHTML = `Aufgabe <button class="btn-delete">X</button>`;
+    return neuesLi;
+}
 
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        
-        // Id des geklickten Btn
-        const buttonId = btn.id; 
-        
-        // Liste zum Btn finden
-        const listeId = buttonId.replace("btn-", "ul-");
-        const liste = document.getElementById(listeId);
-        
-        //prüfen, ob Liste existiert
-        if (liste) { 
-            const neuesLi = document.createElement("li");
-            //damit direkt beschreibbar
-            neuesLi.setAttribute("contentEditable", "true");
-            // X-Button hinzufügen
-            neuesLi.innerHTML = `Aufgabe <button class="btn-delete">X</button>`;
-            liste.appendChild(neuesLi);
+function addTask(event){
+    const btn = event.target;  
+    // Liste zum Btn finden
+    const listeId = btn.id.replace("btn-", "ul-");
+    const liste = document.getElementById(listeId);
+    
+    //prüfen, ob Liste existiert
+    if (liste) { 
+        const neuesLi = createTask();
+        liste.appendChild(neuesLi);
+        //damit Cursor sofort da ist
+        neuesLi.focus();
+    }
+}
 
-            //damit Cursor sofort da ist
-            neuesLi.focus();
-        }
+function removeTask(event){
+    //li vom x-btn finden
+    const li = event.target.closest("li");
+    if (li) li.remove();
+}
 
-    });
+function ausklappen(event){
+    //bereits vorhandene li editierbar
+    if (event.target.tagName === "LI") {
+       //auf Klick Klasse hinzufügen
+       event.target.classList.toggle("ausgeklappt");
+       event.target.setAttribute("contentEditable", "true");
+       event.target.focus();
+      
+       event.target.addEventListener("blur", () => {
+           // Wenn Feld verlassen wird, setze das li zurück
+           event.target.classList.remove("ausgeklappt");
+       });
+   }
+}
+
+//add btn bei Click 
+document.querySelectorAll(".btn-add").forEach(btn => {
+    btn.addEventListener("click", addTask);
 });
 
- 
+//entfernen + ausklappen 
 document.querySelectorAll("ul").forEach(ul => {
     ul.addEventListener("click", (event) => {
         //li löschen
-        if (event.target.classList.contains("btn-delete")) {
-            //li vom x-btn finden
-            const li = event.target.closest("li");
-            if (li) li.remove();
-        }
-
-        //bereits vorhandene li editierbar
-        if (event.target.tagName === "LI") {
-            event.target.setAttribute("contentEditable", "true");
-            event.target.focus();
-
-            //Test, auf Klick soll Li Klasse hinzugefügt werden
-            event.target.classList.toggle("ausgeklappt");
-            event.target.addEventListener("blur", () => {
-                // Wenn der Fokus verloren geht, setze das li zurück
-                event.target.classList.remove("ausgeklappt");
-            });
-        }
-
+        if (event.target.classList.contains("btn-delete")) removeTask(event);
+        ausklappen(event);
     });
 });
-
-
-
-
-
-
-
-
-
-
 
 
