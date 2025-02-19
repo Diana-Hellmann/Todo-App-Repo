4bbcba7f7b1836336d22bbd273bdd1e77b60e3d3
Diana Hellmann <hellmanndiana@googlemail.com>
2025-02-19 10:35:00 +0100

    li ausklappbar, Css Anpassungen

--- script.js
+++ script.js
@@ -12,17 +12,53 @@ buttons.forEach(btn => {
         
         //prüfen, ob Liste existiert
         if (liste) { 
-            const neuesElement = document.createElement("li");
+            const neuesLi = document.createElement("li");
             //damit direkt beschreibbar
-            neuesElement.setAttribute("contentEditable", "true");
-            liste.appendChild(neuesElement);
+            neuesLi.setAttribute("contentEditable", "true");
+            // X-Button hinzufügen
+            neuesLi.innerHTML = `Aufgabe <button class="btn-delete">X</button>`;
+            liste.appendChild(neuesLi);
 
             //damit Cursor sofort da ist
-            neuesElement.focus();
+            neuesLi.focus();
         }
 
     });
 });
+
+ 
+document.querySelectorAll("ul").forEach(ul => {
+    ul.addEventListener("click", (event) => {
+        //li löschen
+        if (event.target.classList.contains("btn-delete")) {
+            //li vom x-btn finden
+            const li = event.target.closest("li");
+            if (li) li.remove();
+        }
+
+        //bereits vorhandene li editierbar
+        if (event.target.tagName === "LI") {
+            event.target.setAttribute("contentEditable", "true");
+            event.target.focus();
+
+            //Test, auf Klick soll Li Klasse hinzugefügt werden
+            event.target.classList.toggle("ausgeklappt");
+            event.target.addEventListener("blur", () => {
+                // Wenn der Fokus verloren geht, setze das li zurück
+                event.target.classList.remove("ausgeklappt");
+            });
+        }
+
+    });
+});
+
+
+
+
+
+
+
+
 
 
 
